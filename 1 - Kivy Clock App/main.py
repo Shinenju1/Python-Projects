eeac7d25040a1206808e68c0f32280338a542a5d
Shinenju1 <cosminarama97@gmail.com>
2022-07-25 23:39:27 +0200

    added main window body and clock

--- 1 - Kivy Clock App/main.py
+++ 1 - Kivy Clock App/main.py
@@ -1,8 +1,27 @@
+import time
 from kivy.app import App
+from kivy.clock import Clock
+from kivymd.uix.label import Label
+from kivy.core.window import Window
+from kivy.uix.boxlayout import BoxLayout
 
-class ClockApp(App):
-    pass
+Window.size = (400, 700)
+
+class myclock(Label):
+    def update(self, *args):
+        self.text = time.asctime()
+class TimeApp(App):
+    def build(self):
+        layout = BoxLayout(orientation="vertical")
+        
+        clock1 = myclock()
+        
+        Clock.schedule_interval(clock1.update, .1)
+        layout.add_widget(clock1) 
+        
+        layout.add_widget(Label(text='Italy'))       
+        return layout
 
 if __name__ == '__main__':
-    ClockApp().run()
+    TimeApp().run()
     
